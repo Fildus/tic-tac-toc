e4f0ec607c477d204432bd932c658b5576b924be
fildus <davidgastaldello@msn.com>
2020-05-23 02:15:32 +0200

    :heavy_plus_sign: add jquery dependency :lipstick: add timeout for flash_messages.html.twig fade

--- assets/front/js/app.js
+++ assets/front/js/app.js
@@ -1,6 +1,7 @@
 require('@fortawesome/fontawesome-free/css/all.min.css')
 require('@fortawesome/fontawesome-free/js/all')
 require('bootstrap')
+import $ from 'jquery'
 import Vue from 'vue'
 import Navbar from './components/Navbar'
 
@@ -8,3 +9,10 @@ new Vue({
     el: '#vue-app',
     components: {Navbar}
 })
+
+setTimeout(function () {
+    const alerts = document.getElementsByClassName('alert')
+    for (let alert of alerts) {
+        $(alert).alert('close')
+    }
+}, 3000)
